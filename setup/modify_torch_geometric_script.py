e50e6623862642a6bb58c4400f37733c339c6e43
Jane Downer <jdowner@hawk.iit.edu>
2024-08-19 15:55:22 -0500

    README changes

--- setup/modify_torch_geometric_script.py
+++ setup/modify_torch_geometric_script.py
@@ -2,10 +2,8 @@ import shutil
 import os
 
 ''' TODO: modify paths '''
-# path/to/torch_geometric
-dir_to_modify = '/opt/homebrew/Caskroom/miniforge/base/envs/graph_backdoor_detection/lib/python3.8/site-packages/torch_geometric'
-# path/to/repo_pub/setup/modified_torch_geometric_files
-modified_files_dir = '/Users/janedowner/Desktop/Desktop/IDEAL/Project_1/repo_pub/setup/modified_torch_geometric_files'
+dir_to_modify = 'path/to/torch_geometric'
+modified_files_dir = 'path/to/repository/setup/modified_torch_geometric_files'
 
 extensions_to_modify = ['experimental.py',
                         'explain/algorithm/base.py',
